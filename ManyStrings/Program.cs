0eba180c18813adbdef7992a226dd027b97a3a7b
KR1PT1CS <mogos2625727@gmail.com>
2025-02-21 13:35:44 +0000

    Added more Strings

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -6,7 +6,23 @@ namespace ManyStrings
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            string welcomeMessage = "Hello my friend:";
+            string playerName = "Txger";
+            char c1 = '\u2623', c2 = '\u26A1';
+            string phrase = $"{welcomeMessage}\n Player: {playerName}\n Do your best\t{c2}";
+            Console.WriteLine(phrase);
+
+            int coins = 10;
+            int coinsNeeded = 100;
+            Console.WriteLine($"You have {coins} dollars. You need ({coinsNeeded} - {xp}) more.");
+
+            double amount = 1.111111;
+            Console.WriteLine($"2 decimal places: {amount:f2}");
+
+            int a = 3;
+            int b = 7;
+            Console.WriteLine($"{a} + {b} is equal to {a + b}.");
+            Console.WriteLine($"The value of x is {a} and the value of y is {b}.");
         }
     }
 }
