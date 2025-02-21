ed27abff88290d12ae1fd036025d3b2fb27a21b1
KR1PT1CS <mogos2625727@gmail.com>
2025-02-21 14:24:40 +0000

    Added the Code for the 4 numbers and percentage

--- Percentagem/Program.cs
+++ Percentagem/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 
 namespace Percentagem
+class Program
 {
-    public class Program
+    static void Main()
     {
-        private static void Main(string[] args)
+        double[] numeros = new double[4];
+        for (int i = 0; i < 4; i++)
         {
-            Console.WriteLine("Hello LP1!");
+            Console.Write($"Digite número {i + 1}: ");
+            numeros[i] = Convert.ToDouble(Console.ReadLine());
+            
+            while (numeros[i] < 0 || numeros[i] > 1)
+            {
+                Console.Write("Número inválido! Digite entre 0 e 1: ");
+                numeros[i] = Convert.ToDouble(Console.ReadLine());
+            }
         }
+
+        Console.WriteLine("\nResultados:");
+        Console.WriteLine("------------------------");
+        
+        foreach (double n in numeros)
+        {
+            Console.WriteLine($"{n,10:F7} | {n,11:P2}");
+        }
+        Console.WriteLine("------------------------");
     }
 }
+            
+          
+
+
+
+
+
+
