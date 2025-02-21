8272757f36030cd05b6b78cbfd6a3d024e90379a
KR1PT1CS <mogos2625727@gmail.com>
2025-02-21 13:30:43 +0000

    Added Boolean Variables

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -15,6 +15,12 @@ namespace VariousTypes
             Console.WriteLine($"\nChars: {c1} {c2} {c3}");
 
             double money = 10.5d;
+            float chance_perc = 0.50f;
+            Console.WriteLine($"\n\nMoney: {money}, chance: {chance_perc}");
+
+            bool true_power = true;
+            bool false_power = false;
+            Console.WriteLine($"Power: {true_power} or {false_power}");
         }
     }
 }
