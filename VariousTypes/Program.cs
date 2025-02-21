ef89065de14927fa125d7de7bb6b8b39241b0dad
KR1PT1CS <mogos2625727@gmail.com>
2025-02-21 13:26:51 +0000

    Added Char Variables

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -10,6 +10,9 @@ namespace VariousTypes
             int xp_needed = 1000000;
             long gold = 10000000000;
             Console.WriteLine($"Power: {power}, xp: {xp_needed}, gold: {gold}")
+
+            char c1 = '\u20AD', c2 = '\u20AD' c3 = '\u20AD';
+            Console.WriteLine($"\nChars: {c1} {c2} {c3}");
         }
     }
 }
