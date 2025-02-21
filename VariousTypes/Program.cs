09cbd2f766756d9fea292bd46f92f516d6c91c37
KR1PT1CS <mogos2625727@gmail.com>
2025-02-21 13:29:54 +0000

    Added Real Variables

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -8,11 +8,13 @@ namespace VariousTypes
         {
             short power = 7;
             int xp_needed = 1000000;
-            long gold = 10000000000;
-            Console.WriteLine($"Power: {power}, xp: {xp_needed}, gold: {gold}")
+            long gold = 10000000000L;
+            Console.WriteLine($"Power: {power}, xp: {xp_needed}, gold: {gold}");
 
-            char c1 = '\u20AD', c2 = '\u20AD' c3 = '\u20AD';
+            char c1 = '\u20AD', c2 = '\u20AD', c3 = '\u20AD';
             Console.WriteLine($"\nChars: {c1} {c2} {c3}");
+
+            double money = 10.5d;
         }
     }
 }
