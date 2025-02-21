dc41e573b51d0f1257f2cdbda62c09325c2fa97b
KR1PT1CS <mogos2625727@gmail.com>
2025-02-21 13:39:22 +0000

    Formatations and Summing everything up

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -6,23 +6,25 @@ namespace ManyStrings
     {
         private static void Main(string[] args)
         {
-            string welcomeMessage = "Hello my friend:";
-            string playerName = "Txger";
-            char c1 = '\u2623', c2 = '\u26A1';
-            string phrase = $"{welcomeMessage}\n Player: {playerName}\n Do your best\t{c2}";
+            double xx = 1.23456;
+            int ii = 19;
+            Console.WriteLine($"2 casas: {xx:f2}");
+            Console.WriteLine($"Percentagem com uma casa decimal: {xx:p1}");
+            Console.WriteLine($"Hexadecimal: {ii:x}");
+            Console.WriteLine($"Moeda: {ii:c}");
+
+            string welcomeMessage = "\nHello my friend:";
+            string playerName = "KR1PT1CS";
+            char c1 = '\u20AD';
+            string phrase = $"{welcomeMessage}\n Player: {playerName}\n Do your best\t{c1}";
             Console.WriteLine(phrase);
 
             int coins = 10;
             int coinsNeeded = 100;
-            Console.WriteLine($"You have {coins} dollars. You need ({coinsNeeded} - {xp}) more.");
+            Console.WriteLine($"\nYou have {coins} dollars. You need ({coinsNeeded - coins}) more.");
 
             double amount = 1.111111;
             Console.WriteLine($"2 decimal places: {amount:f2}");
-
-            int a = 3;
-            int b = 7;
-            Console.WriteLine($"{a} + {b} is equal to {a + b}.");
-            Console.WriteLine($"The value of x is {a} and the value of y is {b}.");
         }
     }
 }
